e638a48c30ad7c11779c51aae93e540c20ece69a
BrbBot69 <148257126+BrbBot69@users.noreply.github.com>
2025-08-09 21:01:18 +0200

    Update yt-stalk.js

--- plugins/yt-stalk.js
+++ plugins/yt-stalk.js
@@ -32,7 +32,7 @@ cmd({
       + `┃🎥 *Videos:* ${yt.video_count}\n`
       + `┃🔗 *Channel Link:* (${yt.channel})\n`
       + `╰━━━⪼\n\n`
-      + `🔹 *Powered by MAFIA ADEEL*`;
+      + `🔹 *Powered by BALLAS𝗑ᴾᴿᴼ*`;
 
     await conn.sendMessage(from, {
       image: { url: yt.avatar },
