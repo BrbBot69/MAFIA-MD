ab5a76e8d9da381d98ce69e9355c1aba4696ed55
BrbBot69 <148257126+BrbBot69@users.noreply.github.com>
2025-08-09 20:56:04 +0200

    Update tool-url.js

--- plugins/tool-url.js
+++ plugins/tool-url.js
@@ -65,7 +65,7 @@ cmd({
       `*${mediaType} Uploaded Successfully*\n\n` +
       `*Size:* ${formatBytes(mediaBuffer.length)}\n` +
       `*URL:* ${mediaUrl}\n\n` +
-      `> © Uploaded by ᴍᴀғɪᴀ ᴀᴅᴇᴇʟ 💜`
+      `> © Uploaded by BALLAS𝗑ᴾᴿᴼ 💜`
     );
 
   } catch (error) {
